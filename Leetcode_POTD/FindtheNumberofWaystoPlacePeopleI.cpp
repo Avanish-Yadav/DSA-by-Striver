d7d823349cca485ba8223917dc2dc4900d1f1615
Avanish-Yadav <avanishyadav1906@gmail.com>
2025-09-02 16:45:53 +0530

    Added the 2 sep potd

---
+++ Leetcode_POTD/FindtheNumberofWaystoPlacePeopleI.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfPairs(vector<vector<int>>& points) {
+        int n=points.size();
+        sort(points.begin(),points.end(),[](const vector<int> &p1,const vector<int> &p2){
+            return p1[0]==p2[0] ? p1[1]>p2[1] : p1[0]<p2[0];
+        });
+        int count=0;
+        for(int A=0;A<n-1;A++){
+            int bottom_right_y=INT_MIN;
+            for(int B=A+1;B<n;B++){
+                if(points[B][1]<=points[A][1] && points[B][1]>bottom_right_y){
+                    count++;
+                    bottom_right_y=points[B][1];
+                }
+            }
+        }
+        return count;
+    }
+int main(){
+    vector<vector<int>> points = {{2, 7},{2, 4},{2, 2},{5, 6},{5, 3},{7, 4}};
+    cout<<numberOfPairs(points);
+}

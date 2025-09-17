40e0423c543241baf4f9bf33634a58f99b84c4c7
Avanish-Yadav <avanishyadav1906@gmail.com>
2025-09-18 01:46:39 +0530

    Added the space optimization in Move Zero to end

--- Day3/Move_Zero_To_End.cpp
+++ Day3/Move_Zero_To_End.cpp
@@ -16,13 +16,42 @@ vector<int> moveZeros(int n,vector<int> &arr){
     }
     return ans;
 }
+//Two pass
+vector<int> moveZeros2(int n,vector<int> &arr){
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            arr[j]=arr[i];
+            j++;
+        }
+    }
+    for(int k=j;k<n;k++){
+        arr[k]=0;
+    }
+    return arr;
+}
+
+vector<int> moveZeros3(int n,vector<int> &arr){
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    return arr;
+}
 int main(){
     vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
     int n = 10;
-    vector<int> ans = moveZeros(n, arr);
+    //Approach 1
+    //vector<int> ans = moveZeros(n, arr);
+    //Approach 2- Two pass
+    //vector<int> res=moveZeros2(n,arr);
+    vector<int> res2=moveZeros3(n,arr);
     cout<<"After Moving "<<endl;
     for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
+        cout<<res2[i]<<" ";
     }
     return 0;
 }
